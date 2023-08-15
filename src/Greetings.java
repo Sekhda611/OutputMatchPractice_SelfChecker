e18d097220d936eaccacffd7fa7ba643d0ef058b
Sonal Sekhda <ssekhda@ggc.edu>
2023-08-15 13:29:49 -0400

    Initial commit

---
+++ src/Greetings.java
@@ -0,0 +1,6 @@
+public class Greetings {
+    public static void main(String[] args) {
+      System.out.println("Good morning!");
+      System.out.println("I love Java because of it's object orientted. ");
+    }
+ }
